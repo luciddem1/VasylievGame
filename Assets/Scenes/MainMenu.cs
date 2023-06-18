faee25306541bc2798cb2f2753dd72e98def2867
Daniil_plt <91540920+luciddem1@users.noreply.github.com>
2023-06-18 22:58:13 +0300

    full game

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void Level1()
+    {
+		SceneManager.LoadScene("Menu1");
+	}
+
+	public void Level2()
+	{
+		SceneManager.LoadScene("Menu2");
+	}
+
+	public void Level3()
+	{
+		SceneManager.LoadScene("Menu3");
+	}
+
+
+
+
+	public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
